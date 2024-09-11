c0388056388663e60cbd6630cd39fd2e634c5a56
cemil3535 <cemilozcan35@gmail.com>
2024-09-11 22:55:29 +0300

    Add project files.

---
+++ BlogApp/Controllers/BlogController.cs
@@ -0,0 +1,54 @@
+﻿using BlogApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        private static int _nextId = 1;
+        private static List<BlogPost> _posts = new List<BlogPost>();
+
+
+        public IActionResult Index()
+        {
+            ViewBag.TotalPosts = _posts.Count;
+            ViewData["PageTitle"] = "Blog Gonderileri";
+
+            return View(_posts);
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.CurrentTime = DateTime.Now;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(BlogPost blogPost)
+        {
+            if (ModelState.IsValid)
+            {
+                blogPost.Id = _nextId++;
+                _posts.Add(blogPost);
+                TempData["SuccessMessage"] = "Blog gonderisi basari ile olusturuldu.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewBag.CurrentTime = DateTime.Now;
+            return View(blogPost);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var post = _posts.FirstOrDefault(x => x.Id == id);
+            if (post is null)
+            {
+                return NotFound();
+            }
+
+            ViewData["CreatedAgo"] = $"{(DateTime.Now - post.CreatedAt).TotalMinutes} dakika once";
+
+            return View(post);
+        }
+    }
+}
---
+++ BlogApp/Models/BlogPost.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.Models
+{
+    public class BlogPost
+    {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="baslik gereklidir.")]
+        [StringLength(100, ErrorMessage ="100 karakterden fazla olamaz.")]
+        public string Title { get; set; }
+
+
+        [Required(ErrorMessage ="Icerik gereklidir.")]
+        public string Content { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    }
+}
